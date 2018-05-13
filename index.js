ac10e16cb368f895155217b2fdb6649eb12213ba
tastycode <kultiv8tor@gmail.com>
2018-05-13 17:02:21 -0500

    remove unused endpoints and logging

--- index.js
+++ index.js
@@ -8,16 +8,11 @@ let authToken = process.env.TWILIO_AUTH_TOKEN
 module.exports = api
 
 api.post('/api/record', async function (request) {
-  console.log('process.env', process.env)
-  console.log('request.body', request.body)
   const recordingUrl = request.post.RecordingUrl
   const callSid = request.post.CallSid
   const recordingDuration = request.post.RecordingDuration
-  console.log({accountSid, authToken})
   let client = new twilio(accountSid, authToken)
   let call = await client.calls(callSid).fetch()
-
-  // store the call and send the user to the call page
 
   await client.messages.create({
     to: call.from,
@@ -25,27 +20,3 @@ api.post('/api/record', async function (request) {
     body: `Your recording is complete: ${recordingUrl}`
   })
 })
-
-
-api.get('/api/recordings/:id', async function (request) {
-  // if queryString param download = true, and the record has a payment ID, then redirect to recording
-})
-
-
-
-api.post('/api/recording_paid', async function (request) {
-  // validate the payment with stripe
-})
-
-/*
- * table: recordings
- * columns: -
- *   url:
- *   from_phone:
- *   to_phone:
- *   duration:
- *   date:
- *   twilio_sid:
- *   stripe_payment_id:
-*/
-
